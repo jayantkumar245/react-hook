b129b55af16365d9b502ec298c1a60e633261cc6
Jayant Kumar <100698593+jayantkumar245@users.noreply.github.com>
2023-04-30 20:46:53 +0530

    Initial commit

---
+++ src/Counter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
